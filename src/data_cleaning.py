c659e3a557e4c34850f54ec8aff78049eade34a7
varunpatnabh <varunpatnabh@gmail.com>
2023-10-24 15:38:03 +0530

    test1

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -12,11 +12,18 @@ def fill_values(df):
     df["Type_of_Cab"] = df["Type_of_Cab"].fillna("F")
 
     # filling "Customer_Since_Months" column with the mean of the same column
-    df['Customer_Since_Months'].fillna(int(df['Customer_Since_Months'].mean()))
+    df['Customer_Since_Months'].fillna(int(df['Customer_Since_Months'].mean()),inplace = True)
 
     #filling 'Var1' column with the mean value of that column
-    df['Var1'].fillna(int(df['Var1'].mean()))
+    df['Var1'].fillna(int(df['Var1'].mean()),inplace = True)
+
+    #As the no. of lifestyle index and confidence life style index has same nan value so assuming it as one different entity
+    df['Life_Style_Index'].fillna(int(df['Life_Style_Index'].mean()),inplace = True)
+    
+    df['Confidence_Life_Style_Index'] = df['Confidence_Life_Style_Index'].fillna("D")
+
     return df
+
 
 
 def drop_col(df):
@@ -25,9 +32,53 @@ def drop_col(df):
         param : Dataframe
         return : dataframe with all meaningfull column
     '''
-    df = df.drop('Trip_ID',axis = 1,inplace = True)
+    cols_to_drop = ['Trip_ID',]
+    df = df.drop(cols_to_drop,axis = 1)
     return df
 
+
+def type_conversion(df):
+    '''
+    This function is to convert columns into appropiate datatype.
+    params:
+        df : Dataframe to be processed
+    return: 
+        Processed Dataframe
+
+    Trip_ID                         object
+    Trip_Distance                  float64
+    Type_of_Cab                     object
+    Customer_Since_Months          float64
+    Life_Style_Index               float64
+    Confidence_Life_Style_Index     object
+    Destination_Type                object
+    Customer_Rating                float64
+    Cancellation_Last_1Month         int64
+    Var1                           float64
+    Var2                             int64
+    Var3                             int64
+    Gender                          object
+    Surge_Pricing_Type               int64
+    '''
+    df = df.astype(
+                    {'Customer_Since_Months': 'int', 
+                    
+                    'Trip_Distance':'float64',
+                    
+                    'Var1':'int',
+                    'Var2':'int',
+                    'Var3':'int',
+                    'Surge_Pricing_Type':'int'}
+                )
+    return df
+
+
+def handle_missing_value(df):
+    '''
+    This function is to handles missing values in  data.
+    '''
+    df.dropna(inplace = True)
+    return df
 
 
 def data_cleaning(df):
@@ -38,6 +89,9 @@ def data_cleaning(df):
             it will directly take the loaded dataframe
     return : Will give cleaned datafram  for further use
     '''
-    df = drop_col(df)
     df = fill_values(df)
+    df = drop_col(df)
+    df = type_conversion(df)
+    df = handle_missing_value(df)
+    
     return df
--- src/load_data.py
+++ src/load_data.py
@@ -6,12 +6,12 @@ import numpy as np
 from datetime import datetime
 
 
-def load_data(input_path):
+def load_data(train_path):
     '''
         This function is to load data in csv format
         params: input path of the csv 
             
         return : dataframe
     '''
-    df = pd.read_csv(input_path)
+    df = pd.read_csv(train_path)
     return df
